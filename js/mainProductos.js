a244a0156cf253aee1277b9b357c9e9c9cc424ba
victoriaareyes <mavireyes2012@gmail.com>
2025-02-22 23:46:57 -0300

    Prueba 2

--- js/mainProductos.js
+++ js/mainProductos.js
@@ -2,8 +2,8 @@ const app = Vue.createApp({
     data() {
         return {
             productos: [],
-            fillProducto:{
-                id:'',
+            fillProducto: {
+                id: '',
                 nombre: '',
                 precio: '',
                 cantStock: '',
@@ -14,48 +14,45 @@ const app = Vue.createApp({
         async Listar() {
             var url = "https://localhost:44346/api/Values/Listar";
 
-            axios.get(url).
-            then(response => {
-                this.productos = response.data;
-            }).
-            catch(
-                error=>{
+            axios.get(url)
+                .then(response => {
+                    this.productos = response.data;
+                })
+                .catch(error => {
                     console.log('Listado incorrecto');
-                }
-            );
+                });
         },
 
-        Grabar()
-        {
-            if(this.fillProducto.id===''){
+        Grabar() {
+            if (this.fillProducto.id === '') {
                 this.GrabarPost();
-
-            }
-            else {
+            } else {
                 this.EditarPut();
             }
         },
+
         GrabarPost() {
             var url = "https://localhost:44346/api/Values/Grabar";
-            axios.post(url,{
-                nombre : this.fillProducto.nombre,
-                precio : this.fillProducto.precio,
-                cantStock : this.fillProducto.cantStock
-            }).
-            then(response => {
-                if(response){
-                     toastr.success('Registro creado correctamente.');
-                    this.Listar();
-                    $("#modalCrearProducto").modal("hide");
-                }
-            }).
-            catch(error=>{
-               console.log('ocurrio un error'+ error)
-            });
+            axios.post(url, {
+                nombre: this.fillProducto.nombre,
+                precio: this.fillProducto.precio,
+                cantStock: this.fillProducto.cantStock
+            })
+                .then(response => {
+                    if (response) {
+                        toastr.success('Registro creado correctamente.');
+                        this.Listar();
+                        $("#modalCrearProducto").modal("hide");
+                    }
+                })
+                .catch(error => {
+                    console.log('Ocurrió un error', error);
+                    toastr.error('Error al crear el producto.');
+                });
             this.LimpiarObjeto();
         },
 
-        Editar(producto){
+        Editar(producto) {
             console.log(producto);
             $("#modalCrearProducto").modal("show");
 
@@ -65,47 +62,109 @@ const app = Vue.createApp({
             this.fillProducto.cantStock = producto.cantStock;
         },
 
-        EditarPut(){
+        EditarPut() {
             var url = "https://localhost:44346/api/Values/Editar";
-            axios.put(url,{
+            axios.put(url, {
                 id: this.fillProducto.id,
-                nombre : this.fillProducto.nombre,
-                precio : this.fillProducto.precio,
-                cantStock : this.fillProducto.cantStock
-            }).then(response => {
-                if(response){
-                    toastr.info('Registro editado correctamente.');
-                    this.Listar();
-                    $("#modalCrearProducto").modal("hide");
-                }
-            }).
-            catch(error=>{
-                console.log('ocurrio un error')
-            });
+                nombre: this.fillProducto.nombre,
+                precio: this.fillProducto.precio,
+                cantStock: this.fillProducto.cantStock
+            })
+                .then(response => {
+                    if (response) {
+                        toastr.info('Registro editado correctamente.');
+                        this.Listar();
+                        $("#modalCrearProducto").modal("hide");
+                    }
+                })
+                .catch(error => {
+                    console.log('Ocurrió un error', error);
+                    toastr.error('Error al editar el producto.');
+                });
             this.LimpiarObjeto();
         },
-        Eliminar(id){
-            var url = "https://localhost:44346/api/Values/Eliminar?id="+id;
-            axios.delete(url).then(response => {
-                if(response){
-                    toastr.warning('Registro eliminado correctamente.');
-                    this.Listar();
-                }
-            }).
-            catch(error=>{
-                console.log('ocurrio un error')
-            });
 
+        Eliminar(id) {
+            var url = "https://localhost:44346/api/Values/Eliminar?id=" + id;
+            axios.delete(url)
+                .then(response => {
+                    if (response) {
+                        toastr.warning('Registro eliminado correctamente.');
+                        this.Listar();
+                    }
+                })
+                .catch(error => {
+                    console.log('Ocurrió un error', error);
+                    toastr.error('Error al eliminar el producto.');
+                });
         },
-        LimpiarObjeto(){
+
+        LimpiarObjeto() {
             this.fillProducto.id = '';
             this.fillProducto.nombre = '';
             this.fillProducto.precio = '';
             this.fillProducto.cantStock = '';
-        }
+        },
+
+        ActualizarStock(id, cantStock) {
+            console.log(id, cantStock);
+            $("#modalActualizarStock").modal("show");
+
+            this.fillProducto.id = id;
+            this.fillProducto.cantStock = cantStock;
+        },
+
+        ActualizarStockPut() {
+            console.log(this.fillProducto); 
+            var url = "https://localhost:44346/api/Values/AgregarStock?id=" + this.fillProducto.id + "&cantStock=" + this.fillProducto.cantStock;
+            axios.put(url, {
+                id: this.fillProducto.id,
+                cantStock: this.fillProducto.cantStock
+            })
+                .then(response => {
+                    if (response) {
+                        toastr.info('Stock actualizado correctamente.');
+                        this.Listar();
+                        setTimeout(() => {
+                            $("#modalActualizarStock").modal("hide");
+                        }, 500); 
+                    }
+                })
+                .catch(error => {
+                    console.log('Ocurrió un error al actualizar stock', error);
+                    toastr.error('Error al actualizar stock.');
+                });
+            this.LimpiarObjeto();
+        },
+
+        Venta(id, cantidad) {
+            if (cantidad <= 0) {
+                toastr.error("La cantidad debe ser mayor a 0.");
+                return;
+            }
+
+            var url = `https://localhost:44346/api/Values/Vender?id=${id}&cantidad=${cantidad}`;
+            axios.put(url, { id: id, cantidad: cantidad })
+                .then(response => {
+                    if (response) {
+                        toastr.success('Venta realizada correctamente.');
+                        this.Listar();
+                        setTimeout(() => {
+                            $("#modalVenta").modal("hide");
+                        }, 500); 
+                    }
+                })
+                .catch(error => {
+                    console.log('Ocurrió un error al realizar la venta', error);
+                    toastr.error('Error al realizar la venta.');
+                });
+            this.LimpiarObjeto();
+        },
     },
+
     created() {
         this.Listar();
     }
+
 });
 app.mount('#crudProductos');
